5290758edde68a2a9b9f6acdc13527d12d1b2be2
Cristina Lugo <cmlugoce@syr.edu>
2018-07-30 16:45:59 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,10 +8,10 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    if @preferences.allow_create_artists
-      @artist = Artist.new
-    else
+    if @preferences && !@preferences.allow_create_artists
       redirect_to artists_path
+    else
+      @artist = Artist.new
     end
   end
 
